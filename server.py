afc80dbd17de4a95d72cdf76483d562070516ef5
trandat <tranmanhdathsgs@gmail.com>
2020-10-20 21:28:10 +0700

    update

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+import time
 import flask
 import requests
 from flask import flash, request, redirect, render_template, \
@@ -12,34 +12,49 @@ app = flask.Flask(__name__)
 
 app.secret_key = "colect_data"
 app.config["DEBUG"] = True
-app.config['UPLOAD_FOLDER'] = os.getcwd() + '/static/demo'
+app.config['UPLOAD_FOLDER'] = os.getcwd() + '/static/audios'
 app.config['MAX_CONTENT_LENGTH'] = 20000 * 1024 * 1024
 
 @app.route('/get_ids', methods=['GET'])
 def get_ids():
+    temp = []
     dict_sentences = get_all_sentences()
-    return jsonify(dict_sentences)
-@app.route('/upload_files', methods=['POST'])
+    for dict_sentence in dict_sentences:
+        temp_id = {}
+        temp_id["id"] = dict_sentence["id"]
+        temp.append(temp_id)
+    return jsonify(temp)
+@app.route('/get_sentence', methods=['POST'])
+def get_sentence():
+    sentence = None
+    id = request.form["id"]
+    dict_sentences = get_all_sentences()
+    for dict_sentence in dict_sentences:
+        if int(id) == int(dict_sentence["id"]):
+            sentence = dict_sentence["sentence"]
+            break
+    return jsonify({"sentence":sentence})
+@app.route('/upload_audios', methods=['POST'])
 def upload_files():
     if request.method == 'POST':
         files = request.files.getlist('file')
         if files.count == 0:
-            print('no file uploading!')
-            return jsonify({'message':'empty file'}), 400
+            flash('No file selected for uploading')
+            return redirect(request.url)
         else:
-            print(len(files))
-            accepted_file = []
+            time_upload = time.time()
+            time_upload = time.localtime(time_upload)
+            time_name = time.strftime("%Y%m%d%H%M", time_upload)
+            folder_upload = os.path.join(str(app.config['UPLOAD_FOLDER']), time_name)
+            if not os.path.exists(folder_upload):
+                os.mkdir(folder_upload)
+            file_names = []
             for file in files:
-                print(file.filename)
                 if file:
                     filename = secure_filename(file.filename)
-                    path_to_file = os.path.join(
-                            str(app.config['UPLOAD_FOLDER']), filename)
+                    path_to_file = os.path.join(folder_upload, filename)
                     file.save(path_to_file)
-                    accepted_file.append(filename)
-            if len(accepted_file)==0:
-                return jsonify({'message':'file extension is not allowed'}), 400
-            else:
-                return jsonify({'message':'upload '+str(len(accepted_file))+' files'}), 200
+                    file_names.append(filename)
+            return jsonify({"upload":"Success"})
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
